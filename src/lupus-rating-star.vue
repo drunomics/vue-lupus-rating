c146c5b011af823371446dd8fb9c1488bab56841
Maximilian Mikus <maximilianmikus@gmail.com>
2018-05-14 20:46:19 +0200

    Implementing most of the component.

---
+++ src/lupus-rating-star.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="lupus-rating-star">
+    <svg width="20px" height="20px" viewBox="0 0 20 20" version="1.1"
+         xmlns="http://www.w3.org/2000/svg">
+      <defs>
+        <clipPath id="cut-off-right">
+          <rect x="11" y="0" width="10" height="20" />
+        </clipPath>
+      </defs>
+      <g id="GRIDS" stroke="none" stroke-width="1" fill="none"
+         fill-rule="evenodd">
+        <g id="starfill"
+           fill-rule="nonzero">
+          <g id="intro">
+            <g id="rating">
+              <g id="ic_star_rate_black_18px">
+                <polygon id="Shape"
+                         points="10 15.2916667 16.1833333 19.7916667 13.8166667 12.525 20 8.125 12.4166667 8.125 10 0.625 7.58333333 8.125 0 8.125 6.18333333 12.525 3.81666667 19.7916667"></polygon>
+              </g>
+            </g>
+          </g>
+        </g>
+        <g id="starfill2"
+           fill-rule="nonzero" clip-path="url(#cut-off-right)" >
+          <g id="intro" >
+            <g id="rating" >
+              <g id="ic_star_rate_black_18px">
+                <polygon id="Shape"
+                         points="10 15.2916667 16.1833333 19.7916667 13.8166667 12.525 20 8.125 12.4166667 8.125 10 0.625 7.58333333 8.125 0 8.125 6.18333333 12.525 3.81666667 19.7916667"></polygon>
+              </g>
+            </g>
+          </g>
+        </g>
+      </g>
+    </svg>
+  </div>
+</template>
+
+<style lang="scss">
+  .lupus-rating-star {
+    #starfill, #starfill2 {
+      fill: #fff;
+      stroke: #eee;
+      stroke-width: 2px;
+    }
+    #starfill2 {
+      display: none;
+    }
+    cursor: pointer;
+    &.voted {
+      #starfill {
+        fill: #000;
+        stroke: #000;
+      }
+    }
+    &.voted-half {
+      #starfill {
+        fill: #000;
+        stroke: transparent;
+      }
+      #starfill2 {
+        display: block;
+      }
+    }
+    &.highlighted {
+      #starfill {
+        fill: yellow;
+        stroke: yellow;
+      }
+    }
+    &.hovered {
+      #starfill {
+        fill: yellow;
+        stroke: yellow;
+      }
+    }
+  }
+</style>
--- src/lupus-rating.vue
+++ src/lupus-rating.vue
@@ -1,15 +1,127 @@
 <template>
-  <div class="lupus-rating">
-    <div>5 Stars</div>
+  <div class="lupus-rating" v-on:mouseleave="hover(false)">
+    <lupus-rating-star v-on:click.native="vote(1)" v-on:mouseenter.native="hover(1)" :class="style1"/>
+    <lupus-rating-star v-on:click.native="vote(2)" v-on:mouseenter.native="hover(2)" :class="style2"/>
+    <lupus-rating-star v-on:click.native="vote(3)" v-on:mouseenter.native="hover(3)" :class="style3"/>
+    <lupus-rating-star v-on:click.native="vote(4)" v-on:mouseenter.native="hover(4)" :class="style4"/>
+    <lupus-rating-star v-on:click.native="vote(5)" v-on:mouseenter.native="hover(5)" :class="style5"/>
+    <span>currentrating: {{ currentrating }} | </span>
+    <span>rating: {{ rating }} | </span>
+    <span>internal_votecount: {{ internal_votecount }} | </span>
+    <span>voted: {{ voted }} | </span>
+    <span>alreadyvoted: {{ alreadyvoted }} | </span>
+    {{ rating }}
+    {{ voted }}
+    {{ votecount }}
+    {{ alreadyvoted }}
   </div>
 </template>
 
 <script>
-export default {
-  name: 'lupus-rating',
-}
+  import LupusRatingStar from './lupus-rating-star.vue';
+
+  export default {
+    name: 'lupus-rating',
+    props: ['currentrating', 'votecount', 'voteurl', 'alreadyvoted'],
+    data() {
+      return  {
+        voted: false,
+        preventvote: false,
+        hovered: false,
+        internal_votecount: false,
+      }
+    },
+    mounted() {
+      if (this.alreadyvoted) {
+        this.voted = this.alreadyvoted;
+        this.preventvote = true;
+      }
+      if (this.votecount) {
+        this.internal_votecount = this.votecount;
+      }
+    },
+    components: {
+      'lupus-rating-star': LupusRatingStar,
+    },
+    computed: {
+      rating() {
+        const integer_part = Math.floor(this.currentrating);
+        const decimal_part = this.currentrating - integer_part;
+        let rating = integer_part;
+
+        if (decimal_part >= 0.3 && decimal_part <= 0.7) {
+          rating = rating + 0.5;
+        }
+        if (decimal_part >= 0.8) {
+          rating = rating + 1;
+        }
+
+        return rating;
+      },
+      style1() {
+        return {
+          'hovered': this.hovered >= 1  && !this.voted,
+          'highlighted': this.voted >= 1,
+          'voted': this.rating >= 1 && !this.voted,
+          'voted-half': this.rating === 0.5  && !this.voted,
+        }
+      },
+      style2() {
+        return {
+          'hovered': this.hovered >= 2  && !this.voted,
+          'highlighted': this.voted >= 2,
+          'voted': this.rating >= 2  && !this.voted,
+          'voted-half': this.rating === 1.5  && !this.voted,
+        }
+      },
+      style3() {
+        return {
+          'hovered': this.hovered >= 3  && !this.voted,
+          'highlighted': this.voted >= 3,
+          'voted': this.rating >= 3  && !this.voted,
+          'voted-half': this.rating === 2.5  && !this.voted,
+        }
+      },
+      style4() {
+        return {
+          'hovered': this.hovered >= 4  && !this.voted,
+          'highlighted': this.voted >= 4,
+          'voted': this.rating >= 4  && !this.voted,
+          'voted-half': this.rating === 3.5  && !this.voted,
+        }
+      },
+      style5() {
+        return {
+          'hovered': this.hovered >= 5  && !this.voted,
+          'highlighted': this.voted >= 5,
+          'voted': this.rating >= 5  && !this.voted,
+          'voted-half': this.rating === 4.5  && !this.voted,
+        }
+      }
+    },
+    methods: {
+      vote(vote) {
+        if (this.preventvote) {
+          return false;
+        }
+        if (this.votecount) {
+          this.internal_votecount = parseInt(this.internal_votecount) + 1;
+        }
+        else {
+          this.internal_votecount = 1
+        }
+        this.preventvote = true;
+        this.voted = vote;
+      },
+      hover(index) {
+        this.hovered = index;
+      }
+    }
+  }
 </script>
 
 <style lang="scss">
-
+  .lupus-rating {
+    display: flex;
+  }
 </style>
