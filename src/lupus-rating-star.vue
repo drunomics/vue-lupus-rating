f0e25ae09a0b7ec6c0feebf0a7c488cbd192d338
Maximilian Mikus <maximilianmikus@gmail.com>
2018-08-22 13:55:40 +0200

    Remove width and height from svg.

--- src/lupus-rating-star.vue
+++ src/lupus-rating-star.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="lupus-rating-star">
-    <svg width="20px" height="20px" viewBox="0 0 20 20" version="1.1"
+    <svg viewBox="0 0 20 20" version="1.1"
          xmlns="http://www.w3.org/2000/svg">
       <defs>
         <clipPath id="cut-off-right">
